9d987a28a942296ab5d6ccbf6073f4066804fe9a
Dom Harrington <dom@harrington-mail.com>
2013-09-16 16:11:51 +0100

    Improve example

--- example/example.js
+++ example/example.js
@@ -4,28 +4,38 @@ var pugpig = require('../pugpig')
   , editionPage = pugpig.editionPage
 
 var container = editionContainer(
-  { title: 'test title'
-  , image: 'image.jpg'
-  , author: 'Dom Harrington'
-  })
+    { title: 'test title'
+    , image: 'image.jpg'
+    , author: 'Dom Harrington'
+    , id: 'tag:example.com,2013-09:books/first-book'
+    })
 
 var edit = edition(
-  { title: 'test title'
-  , summary: 'text summary'
-  , author: 'Dom Harrington'
-  , cover: 'image.jpg'
-  })
+    { title: 'test title'
+    , summary: 'text summary'
+    , author: 'Dom Harrington'
+    , cover: 'image.jpg'
+    , id: 'tag:example.com,2013-09:first-book'
+    })
 
-var page = editionPage(
-  { title: 'test title'
-  , html: '<h1>Test HTML</h1>'
-  , category: 'my great category'
-  })
+var page1 = editionPage(
+    { title: 'page 1'
+    , html: '<h1>This is the first page</h1>'
+    , category: 'introduction'
+    , id: 'tag:example.com,2013-09:first-book:0'
+    })
+  , page2 = editionPage(
+    { title: 'page 2'
+    , html: '<h1>This is the second page</h1>'
+    , category: 'introduction'
+    , id: 'tag:example.com,2013-09:first-book:1'
+    })
 
-// add page to edition
-edit.add(page)
+// add pages to edition
+edit.add(page1)
+edit.add(page2)
 
-edit.publish('test1.xml')
+edit.publish('edition.xml')
 
 // add edition to container
 container.add(edit)
